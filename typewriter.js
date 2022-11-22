9ae92a979195561d8eea2e887d9b929535a49f26
Hilora Dewett <h.dewett@outlook.com>
2022-11-22 14:49:35 -0500

    LINT check

--- typewriter.js
+++ typewriter.js
@@ -3,6 +3,9 @@ let counter = 0;
 
 //will loop through each character and print it after counter + 50ms
 for (const char of sentence) {
-  setTimeout(() => { process.stdout.write(char);}, counter);
+  setTimeout(() => {
+    process.stdout.write(char);
+  }, counter);
   counter += 50;
-};
+}
+
